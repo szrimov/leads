428aab0f920903b34671d7d7169cacd8d51ed114
Шухрат Мамаримов <shuhrat.mamarimov@pitch-it.ru>
2023-05-22 02:03:52 +0300

    1003 стили wrapper, компонент header

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,15 +1,48 @@
 <template lang="pug">
-  footer.header-component
-    div header
+  header.wrapper.header-component
+    commonButton(type="primary icon")
+      | Создать категорию
+      iconPlus
+    commonInput.header__search-input(
+      v-model="query"
+      type="search"
+      @clear="query = ''")
+      | Найти статью
+      template(#icon)
+        component(:is="query ? 'iconClose' : 'iconLoupe'")
 </template>
 
 <script>
+import commonButton from '@/components/common/button'
+import commonInput from '@/components/common/input'
+import iconPlus from '@/components/icons/plus'
+import iconLoupe from '@/components/icons/loupe'
+import iconClose from '@/components/icons/close'
+
 export default {
-  name: 'header-component'
+  name: 'header-component',
+  components: {
+    commonButton,
+    commonInput,
+    iconPlus,
+    iconLoupe,
+    iconClose
+  },
+  data() {
+    return {
+      query: ''
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .header-component {
+  padding-top: 32px;
+  display: flex;
+  gap: 16px;
+  .header__search-input {
+    width: 100%;
+  }
 }
 </style>
