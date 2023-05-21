e0902170485f7c4c48ed20e36da0d7f4c9ae27a3
Шухрат Мамаримов <shuhrat.mamarimov@pitch-it.ru>
2023-05-22 01:08:44 +0300

    1001 ui, компоненты: кнопки, инпут, селект, поиск, категории

---
+++ components/category-button/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  .category-button-component
+    commonButton(
+      type="primary icon"
+      size="small")
+      iconTrash
+    .category-button__title {{ data }}
+</template>
+
+<script>
+import commonButton from '@/components/common/button'
+import commonInput from '@/components/common/input'
+import iconTrash from '@/components/icons/trash'
+
+export default {
+  name: 'category-button-component',
+  components: {
+    commonButton,
+    commonInput,
+    iconTrash
+  },
+  props: {
+    data: {
+      type: String,
+      default: () => ''
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.category-button-component {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  .category-button__title {
+    font-size: 16px;
+    line-height: 24px;
+    color: $color-neutral-primary-text;
+  }
+}
+</style>
---
+++ components/search-select/index.vue
@@ -0,0 +1,94 @@
+<template lang="pug">
+  .search-select-component
+    commonInput.search-select__input(
+      v-model="value"
+      type="select"
+      isSelect
+      :class="{ 'search-select__input--rotate-icon': isVisible }")
+      | Вложенные статьи
+      template(#icon)
+        iconSelectArrow
+    transition(name="fade")
+      ul.search-select__list(
+        v-if="isVisible"
+        v-click-outside="toggleVisibility")
+        li.search-select__item(
+          v-for="item in data"
+          :key="item.id") {{ item.title }}
+</template>
+
+<script>
+import commonInput from '@/components/common/input'
+import iconSelectArrow from '@/components/icons/select-arrow'
+
+export default {
+  name: 'search-select-component',
+  components: {
+    commonInput,
+    iconSelectArrow
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      value: '',
+      isVisible: false
+    }
+  },
+  watch: {
+    value() {
+      if (this.value) {
+        this.isVisible = true
+      } else {
+        this.isVisible = false
+      }
+    }
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-select-component {
+  position: relative;
+  .search-select__input--rotate-icon {
+    /deep/ .icon {
+      svg {
+        transform: rotate(180deg);
+      }
+    }
+  }
+
+  .search-select__list {
+    margin-top: 1px;
+    position: absolute;
+    z-index: 100;
+    top: 46px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 8px 9px;
+    gap: 4px;
+    width: 100%;
+    background: $color-neutral-white;
+    box-shadow: 0px 10px 40px $color-neutral;
+    border-radius: 5px;
+  }
+  .search-select__item {
+    width: 100%;
+    padding: 6px 8px;
+    cursor: pointer;
+    &:hover {
+      background: rgba(48, 52, 70, 0.03);
+    }
+  }
+}
+</style>
