fce8b861b649241a32b80004ae12a7f5ad2eb0e2
Шухрат Мамаримов <shuhrat.mamarimov@pitch-it.ru>
2023-05-22 01:44:12 +0300

    1002 карточка статьи

---
+++ components/article-card/index.vue
@@ -0,0 +1,102 @@
+<template lang="pug">
+  .article-card-component
+    .article-card__wrapper
+      .article-card__likes
+        iconLike
+        span {{ data.likesQuantity }}
+      .article-card__edit
+        iconPen
+    .article-card__banner
+      img(src="@/static/article-card/banner-card.png")
+    .article-card__title {{ data.title }}
+    .article-card__subtitle {{ data.subtitle }}
+</template>
+
+<script>
+import commonButton from '@/components/common/button'
+import iconLike from '@/components/icons/like'
+import iconPen from '@/components/icons/pen'
+
+export default {
+  name: 'article-card-component',
+  components: {
+    commonButton,
+    iconLike,
+    iconPen
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.article-card-component {
+  padding: 16px;
+  max-width: 290px;
+  width: 100%;
+  border: 1px solid $color-neutral-border;
+  border-radius: 10px;
+  .article-card__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .article-card__likes {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    line-height: 15px;
+    color: $color-neutral-placeholder;
+    user-select: none;
+    svg {
+      cursor: pointer;
+      transition: color 0.3s ease;
+      &:hover {
+        color: $color-product-info-primary;
+      }
+      &:active {
+        color: $color-product-info-primary;
+      }
+    }
+  }
+  .article-card__edit {
+    svg {
+      cursor: pointer;
+      transition: color 0.3s ease;
+      &:hover {
+        color: $color-product-info-primary;
+      }
+      &:active {
+        color: $color-product-info-primary;
+      }
+    }
+  }
+  .article-card__banner {
+    margin-top: 12px;
+  }
+  .article-card__title {
+    margin-top: 8px;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    color: $color-neutral-primary-dark;
+  }
+  .article-card__subtitle {
+    margin-top: 8px;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 22px;
+    color: $color-neutral-secondary-text;
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    word-break: break-word;
+    text-overflow: ellipsis;
+  }
+}
+</style>
--- pages/ui.vue
+++ pages/ui.vue
@@ -23,6 +23,11 @@
     searchSelect(:data="options")
     br
     categoryButton(data="Категория 1")
+    br
+    articleCard(
+      v-for="item in articles"
+      :key="item.id"
+      :data="item")
 </template>
 
 <script>
@@ -30,6 +35,7 @@ import commonButton from '@/components/common/button'
 import commonInput from '@/components/common/input'
 import searchSelect from '@/components/search-select'
 import categoryButton from '@/components/category-button'
+import articleCard from '@/components/article-card'
 import iconPlus from '@/components/icons/plus'
 import iconLoupe from '@/components/icons/loupe'
 import iconClose from '@/components/icons/close'
@@ -41,6 +47,7 @@ export default {
     commonInput,
     searchSelect,
     categoryButton,
+    articleCard,
     iconPlus,
     iconLoupe,
     iconClose
@@ -61,6 +68,29 @@ export default {
         {
           title: 'Название статьи 12345',
           id: 3
+        }
+      ],
+      articles: [
+        {
+          likesQuantity: 40,
+          title: 'Заголовок',
+          subtitle:
+            // eslint-disable-next-line max-len
+            'Идейные соображения высшего порядка, а также сложившаяся структура организации играет важную роль в формировании существенных финансовых и административных условий. Равным образом начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки направлений прогрессивного развития.'
+        },
+        {
+          likesQuantity: 40,
+          title: 'Заголовок',
+          subtitle:
+            // eslint-disable-next-line max-len
+            'Идейные соображения высшего порядка, а также сложившаяся структура организации играет важную роль в формировании существенных финансовых и административных условий. Равным образом начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки направлений прогрессивного развития.'
+        },
+        {
+          likesQuantity: 40,
+          title: 'Заголовок',
+          subtitle:
+            // eslint-disable-next-line max-len
+            'Идейные соображения высшего порядка, а также сложившаяся структура организации играет важную роль в формировании существенных финансовых и административных условий. Равным образом начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки направлений прогрессивного развития.'
         }
       ]
     }
